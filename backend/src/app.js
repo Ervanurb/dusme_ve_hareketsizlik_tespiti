21df5cbfda033d90510484cd09c9239cce753c5c
0zeynep <148884590+0zeynep@users.noreply.github.com>
2026-05-21 23:28:01 +0300

    Add authentication routes and protected endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./modules/auth/auth.routes");
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     message: "Düşme ve Hareketsizlik Tespiti API çalışıyor",
   });
 });
+const authGuard = require("./middleware/auth");
 
+app.get("/api/korumali-veri", authGuard, (req, res) => {
+  res.json({ 
+    message: "Tebrikler! Güvenlik duvarını aştın.", 
+    kullanici_bilgileri: req.user 
+  });
+});
 module.exports = app;
